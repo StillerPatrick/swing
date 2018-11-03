04b914caba5232319213779dd076ce49db8acb98
lesstech <hectic.heat@gmail.com>
2018-11-03 12:39:47 +0100

    healthbar

---
+++ Unity/Swinging_Shooter/Assets/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public Slider slider;
+    public GameObject player;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Hook hook = player.GetComponent<Hook>();
+        if (Input.anyKeyDown)
+        {
+            hook.health -= 1;
+        }
+        slider.value = hook.health;
+        if (hook.health == 0) Destroy(player);
+    }
+}
