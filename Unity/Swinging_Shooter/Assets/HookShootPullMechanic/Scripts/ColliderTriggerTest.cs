869160cfddfc7327a9d169ae863430ee35c72647
Richard Achilles <>
2018-11-03 10:47:33 +0100

    removed doubled scripts

--- Unity/Swinging_Shooter/Assets/HookShootPullMechanic/Scripts/ColliderTriggerTest.cs
+++
@@ -1,1 +0,0 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class ColliderTriggerTest : MonoBehaviour {	// Use this for initialization	void Start () {			}		// Update is called once per frame	void Update () {			}    void OnTriggerEnter2D(Collider2D collision)    {        Debug.Log("entered collider");    }}
