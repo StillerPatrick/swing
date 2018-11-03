8a5bfeecf7ec3ed3d8f86d658ce4ad597560aacd
StillerPatrick <p.stiller@ebs-z.de>
2018-11-03 15:13:54 +0100

    my state

--- Unity/Swinging_Shooter/Assets/PizzaSwing/Scripts/PointCounter.cs
+++ Unity/Swinging_Shooter/Assets/PizzaSwing/Scripts/PointCounter.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PointCounter : MonoBehaviour {
+    private int pizza_count;
+    private int boy_count;
 
+    public
 	// Use this for initialization
 	void Start () {
-		
-	}
+        pizza_count = 0;
+        boy_count = 0;
+}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            pizza_count += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            boy_count += 1;
+        }
+
+
+    }
 }
