fc581c7a925d13021843d74f0002033f933e48ba
StillerPatrick <p.stiller@ebs-z.de>
2018-11-03 15:35:04 +0100

    add colider to objects

--- Unity/Swinging_Shooter/Assets/PizzaSwing/Scripts/PointCounter.cs
+++ Unity/Swinging_Shooter/Assets/PizzaSwing/Scripts/PointCounter.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PointCounter : MonoBehaviour {
     private int pizza_count;
     private int boy_count;
 
-    public
-	// Use this for initialization
-	void Start () {
+    public Text PizzaPoints;
+    public Text BoyPoints;
+
+
+    // Use this for initialization
+    void Start () {
         pizza_count = 0;
         boy_count = 0;
 }
@@ -18,12 +21,17 @@ public class PointCounter : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.U))
         {
             pizza_count += 1;
+            PizzaPoints.text = pizza_count.ToString();
         }
 
         if (Input.GetKeyDown(KeyCode.I))
         {
             boy_count += 1;
+            BoyPoints.text = boy_count.ToString();
         }
+
+    
+
 
 
     }
